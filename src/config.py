52e50cc5b28d7e079297f9b896a966864fc19136
Yviel Castillejos <yviel.castillejos@mail.utoronto.ca>
2020-11-05 16:33:10 -0500

    Add files via upload

---
+++ src/config.py
@@ -0,0 +1,21 @@
+# Parameters
+
+misc = dict(
+    path_to_imgs = "/Users/yvielcastillejos/python/bounding_shape/images" , 
+    path_to_save = "/Users/yvielcastillejos/python/bounding_shape/images"
+)
+
+Screen = dict(
+    S_WIDTH = 500,
+    S_HEIGHT = 500,
+    GRIDSIZE = 50
+)
+
+colour = dict(
+    clr2 = (193, 200, 199),
+    clr1 = (127,255,0),
+    selected = ((96, 108, 118)),
+    unselected = (193, 200, 199)
+)
+
+

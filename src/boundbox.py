f6361a8b3a2d97a16f7a48490cb30b611b44bca4
yvielcastillejos <yviel.castillejos@mail.utoronto.ca>
2020-11-08 17:42:20 -0500

    txt to save x,y,r; modify anytime

--- src/boundbox.py
+++ src/boundbox.py
@@ -8,17 +8,15 @@ from scipy import ndimage
 import os
 from buttons import*
 
-def event(surface, pos, todraw,rad, screen, redo, redorad,  index,pictures, prevind):
+def event(surface, pos, todraw,rad, screen, redo, redorad,  index,pictures, prevind, txt, height, width):
     r1 = pygame.Rect((S_HEIGHT+20,0),(170, S_WIDTH+150))
     r2 = pygame.Rect((0,S_WIDTH+20),(S_HEIGHT+20, 170))
     r3 = pygame.Rect((0,0),(20, S_WIDTH+200))
     r4 = pygame.Rect((0,0),(S_HEIGHT+200, 20)) 
     for event in pygame.event.get():
-
        if event.type == pygame.QUIT:
            pygame.quit()
            sys.exit()
-
        elif event.type == pygame.KEYDOWN:
            keys = pygame.key.get_pressed() 
            if (event.key == pygame.K_RETURN):
@@ -31,6 +29,9 @@ def event(surface, pos, todraw,rad, screen, redo, redorad,  index,pictures, prev
                img = img[::-1,:]
                if len(pictures)>0:
                    plt.imsave(f"{dir_save}/{pictures[index]}", img)
+                   with open(f"{txtsave}/{txt[index]}", 'w') as f:
+                       for i in range(len(todraw)):
+                           f.write(f"{int((todraw[i][0]-19)*width/S_WIDTH)}\t{int((todraw[i][1]-19)*height/S_HEIGHT)}\t{int(0.5+rad[i]*width/S_WIDTH)}\n")
            elif (event.key == pygame.K_d):
                if len(todraw)>0:
                    redo+= [todraw[-1]]
@@ -67,6 +68,9 @@ def event(surface, pos, todraw,rad, screen, redo, redorad,  index,pictures, prev
                img = img[::-1,:]
                if len(pictures)>0:
                    plt.imsave(f"{dir_save}/{pictures[index]}", img)
+                   with open(f"{txtsave}/{txt[index]}", 'w') as f:
+                       for i in range(len(todraw)):
+                           f.write(f"{int((todraw[i][0]-19)*width/S_WIDTH)}\t{int((todraw[i][1]-19)*height/S_HEIGHT)}\t{int(0.5+rad[i]*width/S_WIDTH)}\n")
                prevind = index
                index = (index+ 1)%len(pictures)
 
@@ -83,7 +87,7 @@ def event(surface, pos, todraw,rad, screen, redo, redorad,  index,pictures, prev
                pos = event.pos
            else:
                pos3 = event.pos
-               if (pos3[0] > 568 and pos3[0] < 618) and (pos3[1] > 102 and pos3[1] < 152):
+               if (pos3[0] >  S_WIDTH +58 and pos3[0] <  S_WIDTH +118) and (pos3[1] > 102 and pos3[1] < 152):
                    for i in range(0,len(todraw)):
                         pygame.draw.circle(surface, clr1, (todraw[i]), int(rad[i]),bor_size)
                         screen.blit(surface, (0,0))
@@ -97,11 +101,14 @@ def event(surface, pos, todraw,rad, screen, redo, redorad,  index,pictures, prev
                    pygame.display.update()
                    if len(pictures)>0:
                        plt.imsave(f"{dir_save}/{pictures[index]}", img)
+                       with open(f"{txtsave}/{txt[index]}", 'w') as f:
+                           for i in range(len(todraw)):
+                               f.write(f"{int((todraw[i][0]-19)*width/S_WIDTH)}\t{int((todraw[i][1]-19)*height/S_HEIGHT)}\t{int(0.5+rad[i]*width/S_WIDTH)}\n")
                    draw_buttons(surface,100)
                    text_render(surface, surface)
                    screen.blit(surface, (0,0))
                    pygame.display.update()
-               elif (pos3[0] > 568 and pos3[0] < 618) and (pos3[1] > 202 and pos3[1] < 252):
+               elif (pos3[0] >  S_WIDTH +58 and pos3[0] <  S_WIDTH +118) and (pos3[1] > 202 and pos3[1] < 252):
                    if len(todraw)>0:
                        redo+= [todraw[-1]]
                        todraw = todraw[:-1]
@@ -117,7 +124,7 @@ def event(surface, pos, todraw,rad, screen, redo, redorad,  index,pictures, prev
                    text_render(surface, surface)
                    screen.blit(surface, (0,0))
                    pygame.display.update()
-               elif (pos3[0] > 568 and pos3[0] < 618) and (pos3[1] > 302 and pos3[1] < 352):
+               elif (pos3[0] > S_WIDTH + 68 and pos3[0] < S_WIDTH + 118) and (pos3[1] > 302 and pos3[1] < 352):
                    if len(redo) >0:
                        todraw+=[redo[-1]]
                        rad+=[redorad[-1]]
@@ -133,9 +140,11 @@ def event(surface, pos, todraw,rad, screen, redo, redorad,  index,pictures, prev
                    text_render(surface, surface)
                    screen.blit(surface, (0,0))
                    pygame.display.update()
-               elif (pos3[0] > 558 and pos3[0] < 628) and (pos3[1] > 402 and pos3[1] < 427):
+               elif (pos3[0] > S_WIDTH + 58 and pos3[0] < S_WIDTH + 128) and (pos3[1] > 402 and pos3[1] < 427):
                    prevind = index
                    index = (index+ 1)%len(pictures)
+                   for i in range(0,len(todraw)):
+                       pygame.draw.circle(surface, clr1, (todraw[i]), int(rad[i]),bor_size)
                    draw_buttons(surface,5)
                    text_render(surface, surface)
                    screen.blit(surface, (0,0))
@@ -144,9 +153,11 @@ def event(surface, pos, todraw,rad, screen, redo, redorad,  index,pictures, prev
                    text_render(surface, surface)
                    screen.blit(surface, (0,0))
                    pygame.display.update()
-               elif (pos3[0] > 558 and pos3[0] < 628) and (pos3[1] > 452 and pos3[1] < 477):
+               elif (pos3[0] > S_WIDTH + 58 and pos3[0] < S_WIDTH + 128) and (pos3[1] > 452 and pos3[1] < 477):
                    prevind = index
                    index = (index-1)%len(pictures)
+                   for i in range(0,len(todraw)):
+                       pygame.draw.circle(surface, clr1, (todraw[i]), int(rad[i]),bor_size)
                    draw_buttons(surface,6)
                    text_render(surface, surface)
                    screen.blit(surface, (0,0))
@@ -155,7 +166,7 @@ def event(surface, pos, todraw,rad, screen, redo, redorad,  index,pictures, prev
                    text_render(surface, surface)
                    screen.blit(surface, (0,0))
                    pygame.display.update()
-               elif (pos3[0] > 548 and pos3[0] < 628) and (pos3[1] > 34 and pos3[1] < 74):
+               elif (pos3[0] > 548 and pos3[0] < S_WIDTH + 128) and (pos3[1] > 34 and pos3[1] < 74):
                    for i in range(0,len(todraw)):
                         pygame.draw.circle(surface, clr1, (todraw[i]), int(rad[i]),bor_size)
                         screen.blit(surface, (0,0))
@@ -169,6 +180,9 @@ def event(surface, pos, todraw,rad, screen, redo, redorad,  index,pictures, prev
                    pygame.display.update()
                    if len(pictures)>0:
                        plt.imsave(f"{dir_save}/{pictures[index]}", img)
+                       with open(f"{txtsave}/{txt[index]}", 'w') as f:
+                           for i in range(len(todraw)):
+                               f.write(f"{int((todraw[i][0]-19)*width/S_WIDTH)}\t{int((todraw[i][1]-19)*height/S_HEIGHT)}\t{int(0.5+rad[i]*width/S_WIDTH)}\n")
                    prevind = index
                    index = (index+ 1)%len(pictures)
                    draw_buttons(surface,100)
@@ -200,14 +214,38 @@ def event(surface, pos, todraw,rad, screen, redo, redorad,  index,pictures, prev
                     text_render(surface, surface)
                     screen.blit(surface, (0,0))
                     pygame.display.update()
-    return pos, todraw, rad, redo,redorad,  index, prevind
+    return pos, todraw, rad, redo, redorad, index, prevind, txt
 
 
-def folder_img(dir):
-   pictures = os.listdir(dir)
+def folder_img(dir, txt):
+   pictures = sorted(os.listdir(dir))
+   txtfiles = sorted(os.listdir(txtfolder))  
    pic = []
+   txt2 = []
+   txt3 = []
+   # test = [elem[:-3] for elem in pictures]
+   test = [elem[:-3] for elem in txtfiles]
    for picture in pictures:
        if picture[-3:] == "jpg" or picture[-3:] == "png" or picture[-3:] == "peg":
            pic.append(picture)
+   test_jpg = [elem[:-3] for elem in pic]
+   test_jpeg = [elem[:-4] for elem in pic]
+#   print(f"pic is {pic}")
+   for pic2 in pic:
+#       print(pic2[-3:])
+       if (pic2[:-4] not in test) and (pic2[-3:] == "jpg" or pic2[-3:] == "png"):
+           txtfiles.append(f"{pic2[:-4]}.txt")
+       elif pic2[:-5] not in test:
+           txtfiles.append(f"{pic2[:-5]}.txt")
+   txtfiles = sorted(txtfiles)
+ #  print(txtfiles)
+   for txtf in txtfiles:
+       if txtf[-4:] == ".csv" or txtf[-4:] == ".txt":
+           txt2.append(txtf)
+  # print(txt2)
+   for t in txt2:
+       if t[:-3] in test_jpg or t[:-3] in test_jpeg:
+           txt3.append(t)
    print(f" The images you have in the directory are {pic}")
-   return pic
+   print(f" The txt files you have in the directory are {txt3}")
+   return pic, txt3
